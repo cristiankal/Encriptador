ca12b17a2b451a3881bf2e95441bf3cd160efe7c
cristian <tunaaltasuba@gmail.com>
2022-09-03 15:00:26 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+/*La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"*/  
+
+function encriptar(){
+   var texto = document.getElementById("input-texto").value.toLowerCase();
+   var txtCifrado =texto.replace(/e/igm,"enter");
+   var txtCifrado =txtCifrado.replace(/i/igm,"imes");
+   var txtCifrado =txtCifrado.replace(/a/igm,"ai");
+   var txtCifrado =txtCifrado.replace(/o/igm,"ober");
+   var txtCifrado =txtCifrado.replace(/u/igm,"ufat");
+
+   document.getElementById("mensaje").style.backgroundImage="none";
+   document.getElementById("advertencia").style.display="none";
+   document.getElementById("recomendacion").style.display="none";
+   document.getElementById("mensaje").innerHTML=txtCifrado;
+}
+
+function desencriptar(){
+   var texto = document.getElementById("input-texto").value.toLowerCase();
+   var txtCifrado =texto.replace(/enter/igm,"e");
+   var txtCifrado =txtCifrado.replace(/imes/igm,"i");
+   var txtCifrado =txtCifrado.replace(/ai/igm,"a");
+   var txtCifrado =txtCifrado.replace(/ober/igm,"o");
+   var txtCifrado =txtCifrado.replace(/ufat/igm,"u");
+
+   document.getElementById("mensaje").style.backgroundImage="none";
+   document.getElementById("mensaje").innerHTML=txtCifrado;
+
+  
+}
+
+
+function copiar (){
+  mensaje.select();
+  navigator.clipboard.writeText(mensaje.value)
+  
+  alert("texto copiado");
+}
